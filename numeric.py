8450957322fcd39ddbf5e786a33c5e504c43a5ed
Parmeet1591 <meets9991@gmail.com>
2025-03-09 01:57:21 +0530

    Add files via upload

---
+++ numeric.py
@@ -0,0 +1,23 @@
+#print(max(1,2,3,4,5,6,7,8,9,10))
+
+#print(min(10,20,30,40,6))
+
+#print(abs(-2030))
+
+print(pow(3,5))
+
+
+#
+#input('Enter the first number')
+#input('Enter the second number')
+
+#input('Enter the first number')
+#input('Enter the second number')
+
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number:"))
+
+print("addition:",num1+num2)
+print("subtraction:",num1-num2)
+print("Multiplication:",num1*num2)
+print("Division:",num1/num2)
